92c2b946ee82df4b94ac1887517d4a62ffb90c9e
Brandt Cox <brandtleydavis@gmail.com>
2023-04-12 16:11:00 -0600

    adding initials files, js, html, css

---
+++ script.js
@@ -0,0 +1,12 @@
+let APIKey = "dfacead3f9ee1cf92fb953110eeae3a1"; //Kayvon's API key, try to get my own
+
+//create input to search city
+
+// test api endpoints
+
+// current conditions and future conditions
+// show city name, date, icon, temp, humidity, wind speed
+
+// save the city in local storage and append to page
+
+// clicking on city displays the forecast again
